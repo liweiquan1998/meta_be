8e0689916f2248789d8f9c4ce334594023e9918e
wanjinhong <wanjh@chinaexpressair.com>
2022-11-28 09:36:56 +0800

    修改类型：fix 修改描述：后台接口优化

--- app/crud/sku.py
+++ app/crud/sku.py
@@ -6,10 +6,7 @@ from app.crud.basic import update_to_db
 
 def create_sku(db: Session, item: schemas.SkuCreate):
     db_item = models.Sku(**item.dict())
-    if db_item.stock == 0:
-        db_item.status = 0
-    else:
-        db_item.status = 1
+    db_item.status = 1
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
@@ -17,11 +14,7 @@ def create_sku(db: Session, item: schemas.SkuCreate):
 
 
 def update_sku(db: Session, item_id: int, update_item: schemas.SkuUpdate):
-    if update_item.stock == 0:
-        status = 0
-    else:
-        status = 1
-    return update_to_db(update_item=update_item, item_id=item_id, db=db, model_cls=models.Sku,extra=("status",status))
+    return update_to_db(update_item=update_item, item_id=item_id, db=db, model_cls=models.Sku,extra=("status",1))
 
 def get_sku_once(db: Session, item_id: int):
     res: models.Sku = db.query(models.Sku).filter(models.Sku.id == item_id).first()
--- app/routers/business.py
+++ app/routers/business.py
@@ -13,18 +13,19 @@ router_businesses = APIRouter(
 @router_businesses.get("/{business_id}/product_skus",summary="一个商家下的所有商品类型")
 @web_try()
 @sxtimeit
-def get_businesses_product_skus(business:str, params: Params = Depends(), db: Session = Depends(get_db)):
-    return paginate(crud.get_business_product_skus(db,int(business)), params)
+def get_businesses_product_skus(business_id:str,name:str=None,status:int=None,
+                                params: Params = Depends(), db: Session = Depends(get_db)):
+    return paginate(crud.get_business_product_skus(db,int(business_id),name,status), params)
 
 
 @router_businesses.get("/{business_id}/orders")
 @web_try()
 @sxtimeit
-def get_business_orders(business:str,params: Params = Depends(), db: Session = Depends(get_db)):
-    return paginate(crud.get_business_orders(db,int(business)), params)
+def get_business_orders(business_id:str,params: Params = Depends(), db: Session = Depends(get_db)):
+    return paginate(crud.get_business_orders(db,schemas.BusinessPageParams(business_id=int(business_id))), params)
 
 @router_businesses.get("/{business_id}/except_orders")
 @web_try()
 @sxtimeit
-def get_business_except_orders(business:str,params:Params = Depends(), db: Session = Depends(get_db)):
-    return paginate(crud.get_business_except_orders(db,int(business)), params)
+def get_business_except_orders(business_id:str,params:Params = Depends(), db: Session = Depends(get_db)):
+    return paginate(crud.get_business_except_orders(db,int(business_id)), params)
