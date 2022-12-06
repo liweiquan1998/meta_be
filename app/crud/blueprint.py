84e0ac5a77efb929edaf601a510080b7d59da77b
wanjinhong <w1145253034@163.com>
2022-12-06 15:45:21 +0800

    修改类型：feat 修改范围：app 修改描述：蓝图接口

--- app/crud/blueprint.py
+++ app/crud/blueprint.py
@@ -11,9 +11,11 @@ def create_blueprint(db: Session, item: schemas.BlueprintCreate):
     # sourcery skip: use-named-expression
     # 重复店铺名检查
     if db.query(models.BluePrint).filter(models.BluePrint.store_id == item.store_id).first():
-        raise Exception(f"店铺 {item.store_id} 已有保存的蓝图")
+        raise Exception(f"店铺id {item.store_id} 已有保存的蓝图")
     if db.query(models.User).filter(models.User.id == item.creator_id).first() is None:
         raise Exception(f"创建者id {item.creator_id} 不存在")
+    if db.query(models.Store).filter(models.Store.id == item.store_id).first() is None:
+        raise Exception(f"店铺 {item.store_id} 不存在")
     # 创建
     db_item = models.BluePrint(**item.dict(), **{"create_time": int(time.time())})
     db.add(db_item)
