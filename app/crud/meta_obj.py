72df7542b5855909f2f3d7fc2a18bdaa5ea45b48
wanjinhong <w1145253034@163.com>
2022-12-09 09:42:32 +0800

    修改类型：fix 修改范围：app 修改描述：meta_obj 新增的时候用户名重复的问题

--- app/crud/meta_obj.py
+++ app/crud/meta_obj.py
@@ -74,7 +74,8 @@ def create_meta_obj(db: Session, item, creator_id, upload_type=None):
 
     # 重复名称检查
     item.name = is_valid_name(item.name, 10)
-    res: models.MetaObj = db.query(models.MetaObj).filter(models.MetaObj.name == item.name).first()
+    res: models.MetaObj = db.query(models.MetaObj).filter(models.MetaObj.name == item.name,
+                                                          models.MetaObj.creator_id == creator_id).first()
     if res:
         raise Exception(f"物品 {item.name} 已存在")
 
