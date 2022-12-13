4d7740238753886863a625f7a1f3cbd01edbbf61
wanjinhong <w1145253034@163.com>
2022-12-13 11:10:28 +0800

    修改类型：fix 修改范围：app 修改描述：upload

--- app/crud/file.py
+++ app/crud/file.py
@@ -19,7 +19,6 @@ def upload_nfs_file(file):
     file_name = f'{uuid.uuid1()}{Path(file.filename).suffix}'
     # result = Path(f'SceneAssets/{time.strftime("%Y%m", time.localtime())}')
     result = Path('SceneAssets') / f'{time.strftime("%Y%m", time.localtime())}'
-    Path('/mnt').chmod(0o777)
     sys_path = '/mnt/nfs/' / result
     sys_path.mkdir(parents=True, exist_ok=True)
     real_path = sys_path / file_name
