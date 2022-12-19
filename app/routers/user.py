0020ddca696a6bed271a5b3719d8bbfaae7dec57
wanjinhong <w1145253034@163.com>
2022-12-19 11:31:53 +0800

    修改类型：fix 修改范围：app 修改描述： user   websocket

--- app/routers/user.py
+++ app/routers/user.py
@@ -62,4 +62,89 @@ def get_user_id(token: str, db: Session = Depends(get_db)):
     return check_user_id(token, db)
 
 
+# ----
+from typing import Union
+
+from fastapi import (
+    Cookie,
+    Depends,
+    FastAPI,
+    Query,
+    WebSocket,
+    status,
+)
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
+            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+            <button onclick="connect(event)">Connect</button>
+            <hr>
+            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+        var ws = null;
+            function connect(event) {
+                var itemId = document.getElementById("itemId")
+                var token = document.getElementById("token")
+                ws = new WebSocket("ws://frps.retailwell.com:20065/user/ws" + "?token=" + token.value);
+                ws.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
+                event.preventDefault()
+            }
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@router_user.get("/ws_test")
+async def get():
+    return HTMLResponse(html)
+
+
+async def get_token(
+    websocket: WebSocket,
+    token: Union[str, None] = Query(default=None),
+):
+    if token is None:
+        raise Exception(422,'token not found')
+    return token
+
+
+@router_user.websocket("/ws")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    token: str = Depends(get_token),
+):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(
+            f"Session cookie or query token value is: {token}"
+        )
 
