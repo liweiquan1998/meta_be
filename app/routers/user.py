207919222688e22f2336b999622fc676faa8af00
wanjinhong <w1145253034@163.com>
2022-12-19 19:07:44 +0800

    修改类型：fix 修改范围：app 修改描述： 现在靠websocket判断账号占用问题

--- app/routers/user.py
+++ app/routers/user.py
@@ -82,6 +82,9 @@ async def websocket_endpoint(
             data = await websocket.receive_text()
             if data == '0':
                 print(f'客户端正常退出,user={user.name}')
+                user.last_ping = time.time() - int(LOGIN_EXPIRED)
+                db.commit()
+                db.flush()
                 return
             user.last_ping = time.time()
             db.commit()
@@ -93,3 +96,76 @@ async def websocket_endpoint(
             if retry >= int(LOGIN_EXPIRED)//int(PING_INTERVAL):
                 print(f'{retry}次连接失败,客户端关闭,user={user.name},exception:{e}')
                 return
+
+
+
+
+
+
+
+
+
+from typing import Union
+
+from fastapi import (
+    Cookie,
+    Depends,
+    FastAPI,
+    Query,
+    WebSocket,
+    status,
+)
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"/></label>
+            <label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"/></label>
+            <button onclick="connect(event)">Connect</button>
+            <hr>
+            <label>Message: <input type="text" id="messageText" autocomplete="off"/></label>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+        var ws = null;
+            function connect(event) {
+                var itemId = document.getElementById("itemId")
+                var token = document.getElementById("token")
+                ws = new WebSocket("ws://127.0.0.1:8080/user/ws" + "?token=" + token.value);
+                ws.onmessage = function(event) {
+                    var messages = document.getElementById('messages')
+                    var message = document.createElement('li')
+                    var content = document.createTextNode(event.data)
+                    message.appendChild(content)
+                    messages.appendChild(message)
+                };
+                event.preventDefault()
+            }
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@router_user.get("/ws_test")
+async def get():
+    return HTMLResponse(html)
+
+
