080cd1e24cbca690a67381bc1fb41c48440c1aa5
wanjinhong <w1145253034@163.com>
2022-12-20 10:28:01 +0800

    修改类型：fix 修改范围：app 修改描述： 现在靠websocket判断账号占用问题

--- app/crud/user.py
+++ app/crud/user.py
@@ -58,7 +58,7 @@ def login_user(db: Session, item: schemas.UserLogin):
     # 密码错误
     if not verify_password(item.password, res.password_hash):
         raise Exception(401, "用户密码错误")
-    if time.time() - res.last_ping < int(LOGIN_EXPIRED):
+    if time.time() - res.last_ping > int(LOGIN_EXPIRED):
         res.occupied = 0
     # 已被占用
     if res.occupied == 1:
