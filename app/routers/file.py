7cfe0de9394d192eb28bdc767c72f393327b92e9
yuyang <sga_yuuyang@163.com>
2023-02-24 09:51:39 +0800

    update file

--- app/routers/file.py
+++ app/routers/file.py
@@ -34,6 +34,7 @@ def upload_file(file: UploadFile = File(...)):
 @sxtimeit
 def upload_minio_file(file: UploadFile = File(...), params: dict = ...):
     # , user=Depends(check_user)):
+    print(params)
     return crud.upload_minio_file(file)
 
 
