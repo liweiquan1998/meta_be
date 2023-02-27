d0e9fbc5c4249164f4ca92a9f5cff3624bf1e4fa
yuyang <sga_yuuyang@163.com>
2023-02-27 14:35:02 +0800

    update file

--- app/crud/marketing_content.py
+++ app/crud/marketing_content.py
@@ -106,7 +106,7 @@ def delete_marketing_content(db: Session, item_id: int):
 
 def market_audio_content(file, params, db):
     file_byte = file.file.read()
-    file_name = f'{uuid.uuid1()}{Path(file.filename).suffix}'
+    file_name = f'{time.strftime("%d%H%M%S", time.localtime())}{random.randint(1000, 9999)}{Path(file.filename).suffix}'
     result = Path(time.strftime("%Y%m", time.localtime()))
     real_path = result / file_name
     path = FMH.put_file(real_path, file_byte)
