1e3b5528c1efe16ad4d48fa21cd101383a5a0f83
yuyang <sga_yuuyang@163.com>
2023-03-01 19:20:17 +0800

    add class storage

---
+++ app/core/storage/base.py
@@ -0,0 +1,12 @@
+class FileStorage:
+    def get_file_name(self, file_name):
+        pass
+
+    def get_file_uri(self, path_end):
+        pass
+
+    def upload_file(self, file):
+        pass
+
+    def get_file(self, path):
+        pass
