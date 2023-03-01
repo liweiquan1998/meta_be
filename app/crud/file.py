3675dd58dfdb54d86d3ea38198ae9ce15684de52
yuyang <sga_yuuyang@163.com>
2023-03-01 16:19:39 +0800

    update file

--- app/crud/file.py
+++ app/crud/file.py
@@ -1,43 +1,52 @@
 import io
 import time
-import random
 from pathlib import Path
 from typing import List
-import uuid
 import magic
 from fastapi.responses import StreamingResponse
 from app.db.minio import FileHandler
+from app.core.storage.file import Handler
+from configs.setting import config
 from app import models, schemas
 from sqlalchemy.orm import Session
 from app.crud.basic import update_to_db
 from app.common.validation import *
 
+sys_prefix = config.get("nfs", "sys_prefix")
+
 FMH = FileHandler("metaverse")
+handler = Handler()
 
 
 def upload_nfs_file(file):
     file_byte = file.file.read()
-    file_name = f'{uuid.uuid1()}{Path(file.filename).suffix}'
+    # file_name = f'{uuid.uuid1()}{Path(file.filename).suffix}'
+    file_name = handler.get_nfs_name(file.filename)
     result = Path('SceneAssets') / f'{time.strftime("%Y%m", time.localtime())}'
-    sys_path = '/mnt/nfs/' / result
+    # sys_path = '/mnt/nfs/' / result
+    sys_path = sys_prefix / result
     sys_path.mkdir(parents=True, exist_ok=True)
     real_path = sys_path / file_name
     try:
-        with real_path.open('wb') as f:
-            f.write(file_byte)
+        # with real_path.open('wb') as f:
+        #     f.write(file_byte)
+        handler.upload_nfs_file(real_path, file_byte)
         real_path.chmod(0o777)
-        return {'uri': f'/file/nfs/{str(result / file_name)}'}
+        uri = handler.get_nfs_uri(str(result / file_name))
+        return {'uri': uri}
     except Exception as e:
         raise Exception(400, f"上传失败{e}")
 
 
 def upload_minio_file(file):
     file_byte = file.file.read()
-    file_name = f'{time.strftime("%d%H%M%S", time.localtime())}{random.randint(1000, 9999)}{Path(file.filename).suffix}'
+    # file_name = f'{time.strftime("%d%H%M%S", time.localtime())}{random.randint(1000, 9999)}{Path(file.filename).suffix}'
+    file_name = handler.get_minio_name(file.filename)
     result = Path(time.strftime("%Y%m", time.localtime()))
     real_path = result / file_name
     path = FMH.put_file(real_path, file_byte)
-    return {'uri': f'/file/minio/{path}'}
+    uri = handler.get_minio_uri(path)
+    return {'uri': uri}
 
 
 def get_file(path: str):
@@ -52,9 +61,11 @@ def get_file(path: str):
 def get_nfs_file(path: str):
     # 兼容老地址
     path = path.split("metaverse_assets/")[-1]
-    file_path = Path('/mnt/nfs') / Path(path)
-    with file_path.open('rb') as f:
-        file_byte = f.read()
+    # file_path = Path('/mnt/nfs') / Path(path)
+    file_path = Path(sys_prefix) / Path(path)
+    # with file_path.open('rb') as f:
+    #     file_byte = f.read()
+    file_byte = handler.get_nfs_file(file_path)
     content_type = magic.from_buffer(file_byte, mime=True)
     if file_byte:
         return StreamingResponse(io.BytesIO(file_byte), media_type=content_type)
