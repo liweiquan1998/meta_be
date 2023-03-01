c41a222d43a68cf68f299b6ff50da6264fe04528
yuyang <sga_yuuyang@163.com>
2023-03-01 16:22:30 +0800

    update file

--- app/crud/file.py
+++ app/crud/file.py
@@ -20,16 +20,12 @@ handler = Handler()
 
 def upload_nfs_file(file):
     file_byte = file.file.read()
-    # file_name = f'{uuid.uuid1()}{Path(file.filename).suffix}'
     file_name = handler.get_nfs_name(file.filename)
     result = Path('SceneAssets') / f'{time.strftime("%Y%m", time.localtime())}'
-    # sys_path = '/mnt/nfs/' / result
     sys_path = sys_prefix / result
     sys_path.mkdir(parents=True, exist_ok=True)
     real_path = sys_path / file_name
     try:
-        # with real_path.open('wb') as f:
-        #     f.write(file_byte)
         handler.upload_nfs_file(real_path, file_byte)
         real_path.chmod(0o777)
         uri = handler.get_nfs_uri(str(result / file_name))
@@ -40,7 +36,6 @@ def upload_nfs_file(file):
 
 def upload_minio_file(file):
     file_byte = file.file.read()
-    # file_name = f'{time.strftime("%d%H%M%S", time.localtime())}{random.randint(1000, 9999)}{Path(file.filename).suffix}'
     file_name = handler.get_minio_name(file.filename)
     result = Path(time.strftime("%Y%m", time.localtime()))
     real_path = result / file_name
@@ -61,10 +56,7 @@ def get_file(path: str):
 def get_nfs_file(path: str):
     # 兼容老地址
     path = path.split("metaverse_assets/")[-1]
-    # file_path = Path('/mnt/nfs') / Path(path)
     file_path = Path(sys_prefix) / Path(path)
-    # with file_path.open('rb') as f:
-    #     file_byte = f.read()
     file_byte = handler.get_nfs_file(file_path)
     content_type = magic.from_buffer(file_byte, mime=True)
     if file_byte:
