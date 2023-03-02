8e66d69ab1dd0330310b09df87d240de9bb6af2f
yuyang <sga_yuuyang@163.com>
2023-03-02 11:13:24 +0800

    update file

--- app/core/storage/file.py
+++ app/core/storage/file.py
@@ -2,9 +2,10 @@ import io
 import random
 import time
 import uuid
+from typing import Tuple
+
 import magic
 from pathlib import Path
-from fastapi.responses import StreamingResponse
 from app.db.minio import FileHandler
 from configs.setting import config
 
@@ -15,17 +16,17 @@ FMH = FileHandler("metaverse")
 
 
 class NfsStorage(FileStorage):
-    def get_file_name(self, file_name):
+    def get_name(self, file_name) -> str:
         file_name = f'{uuid.uuid1()}{Path(file_name).suffix}'
         return file_name
 
-    def get_file_uri(self, end_nfs):
+    def get_uri(self, end_nfs) -> str:
         uri = '/file/nfs/' + end_nfs
         return uri
 
-    def upload_file(self, file):
+    def upload(self, file) -> dict:
         file_byte = file.file.read()
-        file_name = self.get_file_name(file.filename)
+        file_name = self.get_name(file.filename)
         result = Path('SceneAssets') / f'{time.strftime("%Y%m", time.localtime())}'
         sys_path = nfs_prefix / result
         sys_path.mkdir(parents=True, exist_ok=True)
@@ -34,12 +35,12 @@ class NfsStorage(FileStorage):
             with real_path.open('wb') as f:
                 f.write(file_byte)
             real_path.chmod(0o777)
-            uri = self.get_file_uri(str(result / file_name))
+            uri = self.get_uri(str(result / file_name))
             return {'uir': uri}
         except Exception as e:
             raise Exception(400, f"上传文件失败{e}")
 
-    def get_file(self, path):
+    def get_content(self, path) -> Tuple[io.BytesIO, str]:
         # 兼容老地址
         path = path.split("metaverse_assets/")[-1]
         file_path = Path(nfs_prefix) / Path(path)
@@ -47,34 +48,35 @@ class NfsStorage(FileStorage):
             file_byte = f.read()
         content_type = magic.from_buffer(file_byte, mime=True)
         if file_byte:
-            return StreamingResponse(io.BytesIO(file_byte), media_type=content_type)
+            return io.BytesIO(file_byte), content_type
+            # return StreamingResponse(io.BytesIO(file_byte), media_type=content_type)
         else:
             raise Exception(404, f"文件 {path} 不存在")
 
 
 class MinioStorage(FileStorage):
-    def get_file_name(self, file_name):
+    def get_name(self, file_name) -> str:
         file_name = f'{time.strftime("%d%H%M%S", time.localtime())}{random.randint(1000, 9999)}{Path(file_name).suffix}'
         return file_name
 
-    def get_file_uri(self, end_minio):
+    def get_uri(self, end_minio) -> str:
         uri = '/file/minio/' + end_minio
         return uri
 
-    def upload_file(self, file):
+    def upload(self, file) -> dict:
         file_byte = file.file.read()
-        file_name = self.get_file_name(file.filename)
+        file_name = self.get_name(file.filename)
         result = Path(time.strftime("%Y%m", time.localtime()))
         real_path = result / file_name
         path = FMH.put_file(real_path, file_byte)
-        uri = self.get_file_uri(path)
+        uri = self.get_uri(path)
         return {'uri': uri}
 
-    def get_file(self, path):
+    def get_content(self, path) -> Tuple[io.BytesIO, str]:
         file_byte = FMH.get_file(path)
         content_type = magic.from_buffer(file_byte, mime=True)
         if file_byte:
-            return StreamingResponse(io.BytesIO(file_byte), media_type=content_type)
+            return io.BytesIO(file_byte), content_type
         else:
             raise Exception(404, f"文件 {path} 不存在")
 
