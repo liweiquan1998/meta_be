7bf46ea443cf8ac6d6891b6463b467c8789c7136
yuyang <sga_yuuyang@163.com>
2023-03-06 16:39:38 +0800

    update effects

--- app/crud/effects.py
+++ app/crud/effects.py
@@ -4,13 +4,28 @@ from app import models
 
 
 def create_effect(db: Session, item, user):
-    print(item)
     # 创建特效
     db_item = models.Effects(**item.dict(), **{'create_time': int(time.time()),
                                                'create_id': user.id,
-                                               'status': 1,
+                                               'status': 0,
                                                })
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def get_effect(db: Session, user):
+    db_query = db.query(models.Effects).filter(models.Effects.create_id == user.id)
+    return db_query.order_by(models.Effects.id).all()
+
+
+def delete_effect(db: Session, item_id):
+    # 删除
+    item = db.query(models.Effects).filter(models.Effects.id == item_id).filter()
+    if not item:
+        raise Exception(f"effects {item_id} 不存在")
+    item.status = 1
+    db.commit()
+    db.flush()
+    return True
