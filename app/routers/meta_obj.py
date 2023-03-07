7b07abafc04c5abdfab98baac20a1e1a63e573a3
yuyang <sga_yuuyang@163.com>
2023-03-07 16:32:02 +0800

    update meta_all

--- app/routers/meta_obj.py
+++ app/routers/meta_obj.py
@@ -77,8 +77,9 @@ def get_meta_objs(get_item: schemas.MetaObjGet = Depends(), params: Params = Dep
 @router_meta_obj.get("/all", summary="拿到所有元对象")
 @web_try()
 @sxtimeit
-def get_meta_objs(db: Session = Depends(get_db), params: Params = Depends()):
-    return paginate(crud.get_meta_all(db), params)
+def get_meta_objs():
+    # return paginate(crud.get_meta_all(db), params)
+    return f'xxx'
 
 @router_meta_obj.put("/{item_id}", summary="更新元对象状态")
 @web_try()
