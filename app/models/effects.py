5af22b9b6130e1fab8b463619ffcd84dbe8a5ee5
yuyang <sga_yuuyang@163.com>
2023-03-09 10:32:29 +0800

    update effects

--- app/models/effects.py
+++ app/models/effects.py
@@ -10,5 +10,5 @@ class Effects(BaseModel):
     pkg = Column(String(255), comment='pkg文件地址')
     thumbnail = Column(String(255), comment='缩略图地址')
     create_time = Column(Integer, comment='创建时间')
-    create_id = Column(Integer, comment='创建者id')
+    creator_id = Column(Integer, comment='创建者id')
     status = Column(Integer, comment='逻辑删除：0 正常,1 删除')
