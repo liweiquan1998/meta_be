ee00ecb83f35deca1767b8960c20c6ff9e644303
tiancaicai <2803055574@qq.com>
2023-04-25 21:23:04 +0800

    feat: 修改了素材管理的排序

--- app/crud/meta_obj.py
+++ app/crud/meta_obj.py
@@ -157,7 +157,7 @@ def get_meta_obj_by_creator_id(db: Session, creator_id: int):
 
 def get_meta_objs(db: Session, item: schemas.MetaObjGet):
     # sourcery skip: inline-immediately-returned-variable
-    db_query = db.query(models.MetaObj).order_by(models.MetaObj.create_time)
+    db_query = db.query(models.MetaObj)
     if item.name and item.name != "":
         db_query = db_query.filter(models.MetaObj.name.like(f"%{item.name}%"))
     if item.kind is not None and item.kind != -1:
@@ -174,7 +174,7 @@ def get_meta_objs(db: Session, item: schemas.MetaObjGet):
     if item.creator_id is not None and item.creator_id != -1:
         db_query = db_query.filter(models.MetaObj.creator_id == item.creator_id)
 
-    meta_objs = db_query.all()
+    meta_objs = db_query.order_by(models.MetaObj.create_time).all()
     for mo in meta_objs:
         if mo.aigc:
             mo.aigc = mo.aigc.replace('{', '').replace('}', '').split(',')
---
+++ app/routers/background_images.py
@@ -0,0 +1,40 @@
+# from fastapi import APIRouter, Depends
+# from fastapi_pagination import paginate, Params
+# from sqlalchemy.orm import Session
+# from app import schemas, get_db, crud
+# from app.common.validation import check_user
+# from utils import web_try, sxtimeit
+#
+# router_background_images = APIRouter(
+#     prefix="/background_images",
+#     tags=["background_images-图片视频背景管理"],
+# )
+#
+#
+# @router_background_images.post("/", summary="创建图片视频背景")
+# @web_try()
+# @sxtimeit
+# def create_background_images(item: schemas.BackgroundCreate, db: Session = Depends(get_db), user=Depends(check_user)):
+#     return crud.create_background(db, item)
+#
+#
+# @router_background_images.get("/{background_images_id}", summary="获取图片视频背景")
+# @web_try()
+# @sxtimeit
+# def get_background(background_id: int, db: Session = Depends(get_db), user=Depends(check_user)):
+#     return crud.get_background_once(db, background_id)
+#
+#
+# @router_background_images.get("", summary="获取图片视频背景列表")
+# @web_try()
+# @sxtimeit
+# def get_backgrounds(get_item: schemas.BackgroundGet = Depends(), params: Params = Depends(),
+#                     db: Session = Depends(get_db), user=Depends(check_user)):
+#     return paginate(crud.get_backgrounds(db, get_item), params)
+#
+#
+# @router_background_images.delete("/{background_images_id}", summary="删除图片视频背景")
+# @web_try()
+# @sxtimeit
+# def delete_background(background_id: int, db: Session = Depends(get_db), user=Depends(check_user)):
+#     return crud.delete_background(db, background_id)
