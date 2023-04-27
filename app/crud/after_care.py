17525b19a8a415de0e01922d3ef7e2d4cd58c777
tiancaicai <2803055574@qq.com>
2023-04-27 16:53:34 +0800

    feat: 更改了订单信息的状态信息和记录

--- app/crud/after_care.py
+++ app/crud/after_care.py
@@ -13,6 +13,8 @@ def create_after_care(db: Session, order_id: int,status: int,item: schemas.After
     order_item = order.get_order_once(db, order_id)
     if not order_item:
         raise Exception(407, '创建服务失败，找不到原订单')
+    if order_item.after_care_id:
+        raise Exception(422, '已经创建过异常服务，不可重复')
     db_item.order_id = order_item.id
     db.add(db_item)
     db.commit()
