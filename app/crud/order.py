555841ac3bd312363919b574e4985b729341cbce
tiancaicai <2803055574@qq.com>
2023-04-28 16:32:17 +0800

    feat: 更改了订单信息的状态信息和记录

--- app/crud/order.py
+++ app/crud/order.py
@@ -21,21 +21,21 @@ def update_order(db: Session, item_id: int, update_item: schemas.OrderUpdate):
     original_status = order_db_item.status
     order_db_item.set_field(update_item.dict())
     after_care = after_care_db_item = False
-    if original_status == 4 and order_db_item.status == 4:  # 商家确认退款
+    if original_status == 4 and order_db_item.status == 4:  # 商家拒绝了售后退货
         after_care = True
         after_care_db_item: models.AfterCare = db.query(models.AfterCare). \
             filter(models.AfterCare.id == order_db_item.after_care_id).first()
         after_care_db_item.status = 4
         after_care_db_item.remark = update_item.remark
         order_db_item.close_time = time.time()
-    if original_status == 4 and order_db_item.status == 4:  # 商家确认退款
+    if original_status == 4 and order_db_item.status == 4:  # 商家同意了售后退货
         after_care = True
         after_care_db_item: models.AfterCare = db.query(models.AfterCare). \
             filter(models.AfterCare.id == order_db_item.after_care_id).first()
         after_care_db_item.set_field(update_item.dict())
         after_care_db_item.status = 2
         order_db_item.close_time = time.time()
-    if original_status == 5 and order_db_item.status == 5:  # 确认退款
+    if original_status == 4 and order_db_item.status == 4:  # 商家退款成功
         after_care = True
         after_care_db_item: models.AfterCare = db.query(models.AfterCare). \
             filter(models.AfterCare.id == order_db_item.after_care_id).first()
