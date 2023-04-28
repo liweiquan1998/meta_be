f97897473e847e3f543683117f155d50fc977b7a
tiancaicai <2803055574@qq.com>
2023-04-28 16:16:48 +0800

    feat: 更改了订单信息的状态信息和记录

--- app/models/after_care.py
+++ app/models/after_care.py
@@ -7,20 +7,21 @@ from app.models.database import BaseModel,Base
 
 class AfterCare(BaseModel):
     __tablename__ = "after_care"
-    id = Column(Integer, primary_key=True, comment='id',autoincrement=True)
-    except_type = Column(VARCHAR(30),comment='异常类型',default='退货退款')
-    status = Column(Integer,comment='服务状态',default=0)
-    create_time = Column(Integer,comment='下单时间')
-    deliver_time = Column(Integer,comment='退货时间')
-    recv_time = Column(Integer,comment='收货时间')
-    close_time = Column(Integer,comment='关闭时间')
-    order_id = Column(Integer,comment='订单ID')
-    logistics_order_id = Column(Integer,comment='物流号')
-    logistics_id = Column(Integer,comment='物流商家ID')
-    business_id = Column(Integer,comment='卖家id',index=True)
-    back_reason = Column(VARCHAR(400),comment='退款原因')
-    remark = Column(VARCHAR(400),comment='退款备注')
-    back_cost = Column(Float,comment='退款金额')
+    id = Column(Integer, primary_key=True, comment='id', autoincrement=True)
+    except_type = Column(VARCHAR(30), comment='异常类型', default='退货退款')
+    status = Column(Integer, comment='服务状态', default=0)
+    create_time = Column(Integer, comment='下单时间')
+    deliver_time = Column(Integer, comment='退货时间')
+    recv_time = Column(Integer, comment='收货时间')
+    close_time = Column(Integer, comment='关闭时间')
+    order_id = Column(Integer, comment='订单ID')
+    logistics_order_id = Column(Integer, comment='物流号')
+    logistics_id = Column(Integer, comment='物流商家ID')
+    logistics_name = Column(VARCHAR(15), comment='物流商家名称')
+    business_id = Column(Integer, comment='卖家id', index=True)
+    back_reason = Column(VARCHAR(400), comment='退款原因')
+    remark = Column(VARCHAR(400), comment='退款备注')
+    back_cost = Column(Float, comment='退款金额')
 
     @classmethod
     def get_status_define(cls):
