c04ff7f376ab193936ca587221f009d8d0d5ad42
tiancaicai <2803055574@qq.com>
2023-05-10 19:47:34 +0800

    feat: 更改了直播间排序方式

--- app/routers/file.py
+++ app/routers/file.py
@@ -16,9 +16,9 @@ router_file = APIRouter(
 @router_file.post("/NfsFile/{status}", summary="nfs上传文件")
 @web_try()
 @sxtimeit
-def upload_file(status: int,file: UploadFile = File(...)):
+def upload_file(status: int, file: UploadFile = File(...)):
     # , user=Depends(check_user)):
-    return crud.upload_nfs_file(file,status)
+    return crud.upload_nfs_file(file, status)
 
 
 @router_file.post('/MinioFile', summary="minio上传文件")
@@ -46,11 +46,11 @@ async def create_files(files: List[UploadFile] = File(...)):
 
 
 @router_file.post("/NfsFiles/{status}", summary="nfs上传多个文件")
-async def create_files(status: int,files: List[UploadFile] = File(...)):
+async def create_files(status: int, files: List[UploadFile] = File(...)):
     # , user=Depends(check_user)):
     if not status:
         status = 0
     with ThreadPoolExecutor(max_workers=8) as executor:
-        results = executor.map(crud.upload_nfs_file, (files,status))
+        results = executor.map(crud.upload_nfs_file, (files, status))
     uris = [result['uri'] for result in results]
     return {'uris': uris}
