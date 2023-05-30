9bb4611647d555d6b83e51fea4ae44cfd6e2245c
liweiquan <liweiquan1998@163.com>
2023-05-30 17:45:26 +0800

    add restriction on tag

--- app/schemas/scene.py
+++ app/schemas/scene.py
@@ -39,7 +39,7 @@ class SceneCreate(SceneBase):
 
 class SceneGet(BaseModel):
     name: Union[str, None] = None
-    tag: Union[int, None] = None
+    tag: Union[int, None] = Query(default=None, ge=0, le=2)
     stage: int = Query(default=..., ge=0, le=2)
 
 
