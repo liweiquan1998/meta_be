3b6f79c66d749e3290c0610dc660bfafeb957f29
root <2050669795@qq.com>
2023-06-06 15:45:15 +0800

    修改了不同用户不能创建同名虚拟人的问题和编辑虚拟人时的重名校验【编辑时，同一个用户不能重名，但是不同用户可以重名】

--- app/crud/virtual_human.py
+++ app/crud/virtual_human.py
@@ -1,4 +1,6 @@
 import time
+from typing import List
+
 from app import models, schemas
 from sqlalchemy.orm import Session
 from app.crud.basic import update_to_db
@@ -6,8 +8,10 @@ from app.crud.basic import update_to_db
 
 # 校验虚拟人名字
 def check_virtual_human_name(db: Session, item: schemas.VirtualHumanCheckName, user: models.User):
-    one_virtual_human = db.query(models.VirtualHuman).filter(models.VirtualHuman.name == item.name).filter(models.VirtualHuman.creator_id == user.id).first()
-    if (item.id is None and one_virtual_human is not None) or (item.id is not None and one_virtual_human is not None and one_virtual_human.id != item.id):
+    one_virtual_human = db.query(models.VirtualHuman).filter(models.VirtualHuman.name == item.name).filter(
+        models.VirtualHuman.creator_id == user.id).first()
+    if (item.id is None and one_virtual_human is not None) or (
+            item.id is not None and one_virtual_human is not None and one_virtual_human.id != item.id):
         raise Exception(f'虚拟人名字已存在')
     return True
 
@@ -17,7 +21,8 @@ def create_virtual_human(db: Session, item: schemas.VirtualHumanCreate, user: mo
         raise Exception(f'虚拟人性别出错 实为{item.sex} 应为 0:未知 1:男 2:女')
     if item.status not in [0, 1]:
         raise Exception(f'虚拟人状态出错 实为{item.status} 应为 0:禁用 1:启用')
-    one_virtual_human = db.query(models.VirtualHuman).filter(models.VirtualHuman.name == item.name).first()
+    # print(user.id)
+    one_virtual_human = db.query(models.VirtualHuman).filter(models.VirtualHuman.name == item.name,models.VirtualHuman.creator_id == user.id).first()
     if one_virtual_human:
         raise Exception(f'虚拟人名字已存在')
     # 创建
@@ -30,7 +35,10 @@ def create_virtual_human(db: Session, item: schemas.VirtualHumanCreate, user: mo
     return db_item
 
 
-def update_virtual_human(db: Session, item_id: int, update_item: schemas.VirtualHumanUpdate):
+def update_virtual_human(db: Session, item_id: int, update_item: schemas.VirtualHumanUpdate,user: models.User):
+    one_virtual_human = db.query(models.VirtualHuman).filter(models.VirtualHuman.name == update_item.name,models.VirtualHuman.creator_id == user.id).first()
+    if one_virtual_human:
+        raise Exception(f'虚拟人名字已存在')
     return update_to_db(update_item=update_item, db=db, item_id=item_id, model_cls=models.VirtualHuman)
 
 
